6cdf8a81d1a7377f91dcf3e2d224ced430fe37fe
Ethan Sansom <99099398+EthanSansom@users.noreply.github.com>
2024-05-31 09:11:45 -0400

    Implement formatter generators `bg`, `color`, `emph`, and `wrap`

---
+++ R/style.R
@@ -0,0 +1,142 @@
+# cli --------------------------------------------------------------------------
+
+bg <- function(x) {
+  switch(
+    tolower(check_is_string(x)),
+    black0 = ,
+    black = cli::bg_black,
+    blue0 = ,
+    blue = cli::bg_blue,
+    cyan0 = ,
+    cyan = cli::bg_cyan,
+    green0 = ,
+    green = cli::bg_green,
+    magenta0 = ,
+    magenta = cli::bg_magenta,
+    red0 = ,
+    red = cli::bg_red,
+    white0 = ,
+    white = cli::bg_white,
+    yellow0 = ,
+    yellow = cli::bg_yellow,
+    black1 = ,
+    bright_black = ,
+    black_br = ,
+    br_black = cli::bg_br_black,
+    blue1 = ,
+    bright_blue = ,
+    blue_br = ,
+    br_blue = cli::bg_br_blue,
+    cyan1 = ,
+    bright_cyan = ,
+    cyan_br = ,
+    br_cyan = cli::bg_br_cyan,
+    green1 = ,
+    bright_green = ,
+    green_br = ,
+    br_green = cli::bg_br_green,
+    magenta1 = ,
+    bright_magenta = ,
+    magenta_br = ,
+    br_magenta = cli::bg_br_magenta,
+    red1 = ,
+    bright_red = ,
+    red_br = ,
+    br_red = cli::bg_br_red,
+    white1 = ,
+    bright_white = ,
+    white_br = ,
+    br_white = cli::bg_br_white,
+    yellow1 = ,
+    bright_yellow = ,
+    yellow_br = ,
+    br_yellow = cli::bg_br_yellow,
+    cli::cli_abort(
+      "`x = {.val {x}}` is not a supported background colour.",
+      class = "vlightr_error"
+    )
+  )
+}
+
+background <- bg
+
+color <- function(x) {
+  switch(
+    tolower(check_is_string(x)),
+    black0 = ,
+    black = cli::col_black,
+    blue0 = ,
+    blue = cli::col_blue,
+    cyan0 = ,
+    cyan = cli::col_cyan,
+    green0 = ,
+    green = cli::col_green,
+    magenta0 = ,
+    magenta = cli::col_magenta,
+    red0 = ,
+    red = cli::col_red,
+    white0 = ,
+    white = cli::col_white,
+    yellow0 = ,
+    yellow = cli::col_yellow,
+    black1 = ,
+    bright_black = ,
+    black_br = ,
+    br_black = cli::col_br_black,
+    blue1 = ,
+    bright_blue = ,
+    blue_br = ,
+    br_blue = cli::col_br_blue,
+    cyan1 = ,
+    bright_cyan = ,
+    cyan_br = ,
+    br_cyan = cli::col_br_cyan,
+    green1 = ,
+    bright_green = ,
+    green_br = ,
+    br_green = cli::col_br_green,
+    magenta1 = ,
+    bright_magenta = ,
+    magenta_br = ,
+    br_magenta = cli::col_br_magenta,
+    red1 = ,
+    bright_red = ,
+    red_br = ,
+    br_red = cli::col_br_red,
+    white1 = ,
+    bright_white = ,
+    white_br = ,
+    br_white = cli::col_br_white,
+    yellow1 = ,
+    bright_yellow = ,
+    yellow_br = ,
+    br_yellow = cli::col_br_yellow,
+    cli::cli_abort(
+      "`x = {.val {x}}` is not a supported background colour.",
+      class = "vlightr_error"
+    )
+  )
+}
+
+colour <- color
+
+# misc -------------------------------------------------------------------------
+
+wrap <- function(left = "[", right = "]") {
+  left <- check_is_string(left)
+  right <- check_is_string(right)
+  return(function(x) paste0(left, x, right))
+}
+
+# Wrap in "<>", bold, and highlight (provide emphasis even if no color is available)
+emph <- function(wrap = TRUE, bg = TRUE, bold = TRUE) {
+  call <- rlang::sym("x")
+  if (check_is_bool(wrap)) call <- rlang::call2("paste0", "[", call, "]", .ns = "base")
+  if (check_is_bool(bold)) call <- rlang::call2("style_bold", call, .ns = "cli")
+  if (check_is_bool(bg)) call <- rlang::call2("bg_br_yellow", call, .ns = "cli")
+  rlang::new_function(
+    args = rlang::pairlist2(x = ),
+    body = call,
+    env = rlang::caller_env()
+  )
+}
