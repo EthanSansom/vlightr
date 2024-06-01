d5e0966ba7e2768695939cd53f53cb148ef442de
Ethan Sansom <99099398+EthanSansom@users.noreply.github.com>
2024-06-01 12:55:07 -0400

    Implement `order_ragged()` for use in `describe_highlight()`

--- R/utils-misc.R
+++ R/utils-misc.R
@@ -3,3 +3,12 @@
 `%!|%` <- function(lhs, rhs) if (is.null(lhs)) lhs else rhs
 
 `%||%` <- function(lhs, rhs) if (is.null(lhs)) rhs else lhs
+
+# Inspired by: https://stackoverflow.com/questions/33756754/sorting-a-list-of-unequal-size-vectors-in-r
+order_ragged <- function(x) {
+  x_unragged <- do.call(rbind.data.frame, lapply(x, `length<-`, max(lengths(x))))
+  do.call(order2, x_unragged)
+}
+
+# Allows shorted vectors to be ordered first in `sort_ragged`
+order2 <- function(...) order(..., na.last = FALSE)
