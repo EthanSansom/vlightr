2bbcf702d681e224cbff134bd33cc2b294afde78
Ethan Sansom <99099398+EthanSansom@users.noreply.github.com>
2024-06-02 12:14:03 -0400

    Export styling functions

--- R/style.R
+++ R/style.R
@@ -1,5 +1,6 @@
 # cli --------------------------------------------------------------------------
 
+#' @export
 bg <- function(x) {
   switch(
     tolower(check_is_string(x)),
@@ -51,6 +52,7 @@ bg <- function(x) {
     bright_yellow = ,
     yellow_br = ,
     br_yellow = cli::bg_br_yellow,
+    # TODO: Use `cli::make_ansi_color` here!!!
     cli::cli_abort(
       "`x = {.val {x}}` is not a supported background colour.",
       class = "vlightr_error"
@@ -58,8 +60,10 @@ bg <- function(x) {
   )
 }
 
+#' @export
 background <- bg
 
+#' @export
 color <- function(x) {
   switch(
     tolower(check_is_string(x)),
@@ -111,6 +115,7 @@ color <- function(x) {
     bright_yellow = ,
     yellow_br = ,
     br_yellow = cli::col_br_yellow,
+    # TODO: Use `cli::make_ansi_color` here!!!
     cli::cli_abort(
       "`x = {.val {x}}` is not a supported background colour.",
       class = "vlightr_error"
@@ -118,17 +123,35 @@ color <- function(x) {
   )
 }
 
+#' @export
 colour <- color
 
 # misc -------------------------------------------------------------------------
 
+# TODO Implement:
+# `x` is a string with instructions on how to make a styled vector.
+# `style` is a highlighter generator like checkmate `qassert`.
+# "{_/*[col][bg_col]}"
+# - initial symbols are emphasis `_` -> underling, `/` -> italics, `*` -> bold, `-` -> strike-through
+# - first [] is color (leave blank for no color) and second [] is background
+# "prefix{...}suffix"
+# - if [] or []
+# - test outside of the opening bracket is pasted around the text
+# - "<{}>" -> paste0("<", x, ">"), "<{[blue]}> -> cli::cli_col_blue(paste0("<", x, ">"))
+
+style <- function(x) {
+
+}
+
+#' @export
 wrap <- function(left = "[", right = "]") {
   left <- check_is_string(left)
   right <- check_is_string(right)
   return(function(x) paste0(left, x, right))
 }
 
-# Wrap in "<>", bold, and highlight (provide emphasis even if no color is available)
+# Wrap in "[]", bold, and highlight (provide emphasis even if no color is available)
+#' @export
 emph <- function(wrap = TRUE, bg = TRUE, bold = TRUE) {
   call <- rlang::sym("x")
   if (check_is_bool(wrap)) call <- rlang::call2("paste0", "[", call, "]", .ns = "base")
