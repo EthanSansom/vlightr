f7b91ad16f67bd9837ac91df557594934c8f787d
Ethan Sansom <99099398+EthanSansom@users.noreply.github.com>
2024-06-05 07:13:58 -0400

    Add user specified `n` max print and `align` alignment options to `print()`.

--- R/print.R
+++ R/print.R
@@ -1,15 +1,72 @@
+
+
 #' @export
-print.vlightr_highlight <- function(x, ...) {
+print.vlightr_highlight <- function(
+    x,
+    ...,
+    n = getOption("max.print"),
+    align = c("left", "right", "center")
+  ) {
+
   # Defining the entire `print` method so that `format(x)` can error before
   # `vctrs::obj_print_header(x)` has printed the header.
   formatted <- format(x, .x_name = rlang::caller_arg(x))
-  x_data <- get_data(x)
+  n <- check_is_count(n)
+  n_fmt <- length(formatted)
+  if (n_fmt > n) {
+    length(formatted) <- n
+    print_more <- cli::col_silver(cli::format_inline(
+      "[ Omitted {n_fmt - n} element{?s}. ",
+      "Use {.code print(n = ...)} to see more. ]"
+    ))
+  } else {
+    print_more <- NULL
+  }
 
-  align <- if (is.character(x_data)) "left" else "right"
   vctrs::obj_print_header(x)
-  print_ansi(formatted, align = align, encode_string = is.character(x_data))
+  print_ansi(
+    x = formatted,
+    align = rlang::arg_match(align),
+    encode_string = is.character(get_data(x)),
+    suffix = print_more
+  )
   invisible(x)
 }
+
+
+# TODO: Fix initial white space!! See below for kind of weird printing. Maybe fix
+# spacing as well. Not sure what's going on... OHH. Maybe make left justified for
+# none-characters. I think that's the problem.
+#
+# library(lubridate)
+# work_week_starts <- ymd("2020-01-05") + days(c(0, 8, 16))
+# work_weeks <- interval(work_week_starts, work_week_starts + days(5:7))
+# work_weeks_hl <- highlight_case(
+#   work_weeks,
+#   weekdays(int_start(.x)) == "Monday" ~ \(x) paste(x, "[Bad Start]"),
+#   .x / days(1) > 5 ~ \(x) paste(x, "[Tough Week]"),
+#   .format_once = FALSE
+# )
+# format(work_weeks_hl)
+# print(work_weeks_hl)
+#
+# SEE ALSO
+#
+# x_multi <- highlight(
+#   c(13, 1, 4, 6),
+#   conditions = list(
+#     ~ .x %% 2 == 0,
+#     ~ .x %% 2 != 0,
+#     ~ .x > 10
+#   ),
+#   formatters = list(
+#     ~ paste(.x, "is even"),
+#     ~ paste(.x, "is odd"),
+#     ~ paste(.x, "is big and")
+#   ),
+#   last_formatter = wrap("[", "]")
+# )
+# print(x_multi)
 
 print_ansi <- function(
     x,
@@ -17,8 +74,9 @@ print_ansi <- function(
     sep = " ",
     align = c("left", "right", "center"),
     encode_string = FALSE,
+    suffix = NULL,
     error_call = rlang::caller_env()
-) {
+  ) {
 
   if (!is.character(x)) {
     stop_internal("`x` must be a character, not {.obj_type_friendly {x}}.")
@@ -37,7 +95,9 @@ print_ansi <- function(
     return(invisible())
   }
 
-  if (encode_string) x <- encodeString(x, quote = '"')
+  # TODO: `encodeString` escapes the backslashes in ANSI escape sequences. Find
+  #       a way to get around this.
+  if (encode_string) x <- paste0('"', x, '"')
   # `nchar(NA)` is NA, so treating NA values as the string "NA" instead. This overwrites
   # bare NA values as formatted NA's (ex. `cli::col_red(NA)`, `paste0("<", NA, ">")`)
   # are not themselves NA.
@@ -62,6 +122,7 @@ print_ansi <- function(
   lines <- vapply(chunk(elements, n_per_line), paste, character(1L), collapse = sep)
 
   writeLines(paste(prefixes, lines, sep = sep))
+  if (!is.null(suffix)) cli::cat_line(suffix)
 }
 
 # Thanks: https://stackoverflow.com/questions/3318333/split-a-vector-into-chunks
