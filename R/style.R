3c605345a2b9b1083a1ac817c3eb9561c835ab6b
Ethan Sansom <99099398+EthanSansom@users.noreply.github.com>
2024-06-27 08:54:01 -0400

    Reduce the number of strings accepted as a valid style in `bg`, `color`, and `style`

--- R/style.R
+++ R/style.R
@@ -52,57 +52,32 @@ NULL
 #' @name stylers
 #' @export
 bg <- function(x) {
-  x <- tolower(check_is_string(x))
   switch(
-    x,
-    black0 = ,
-    black = cli::bg_black,
-    blue0 = ,
-    blue = cli::bg_blue,
-    cyan0 = ,
-    cyan = cli::bg_cyan,
-    green0 = ,
-    green = cli::bg_green,
-    magenta0 = ,
-    magenta = cli::bg_magenta,
-    red0 = ,
-    red = cli::bg_red,
-    white0 = ,
-    white = cli::bg_white,
-    yellow0 = ,
-    yellow = cli::bg_yellow,
-    black1 = ,
+    check_is_string(x),
+    black = cli::col_black,
+    blue = cli::col_blue,
+    cyan = cli::col_cyan,
+    green = cli::col_green,
+    magenta = cli::col_magenta,
+    red = cli::col_red,
+    white = cli::col_white,
+    yellow = cli::col_yellow,
     bright_black = ,
-    black_br = ,
-    br_black = cli::bg_br_black,
-    blue1 = ,
+    br_black = cli::col_br_black,
     bright_blue = ,
-    blue_br = ,
-    br_blue = cli::bg_br_blue,
-    cyan1 = ,
+    br_blue = cli::col_br_blue,
     bright_cyan = ,
-    cyan_br = ,
-    br_cyan = cli::bg_br_cyan,
-    green1 = ,
+    br_cyan = cli::col_br_cyan,
     bright_green = ,
-    green_br = ,
-    br_green = cli::bg_br_green,
-    magenta1 = ,
+    br_green = cli::col_br_green,
     bright_magenta = ,
-    magenta_br = ,
-    br_magenta = cli::bg_br_magenta,
-    red1 = ,
+    br_magenta = cli::col_br_magenta,
     bright_red = ,
-    red_br = ,
-    br_red = cli::bg_br_red,
-    white1 = ,
+    br_red = cli::col_br_red,
     bright_white = ,
-    white_br = ,
-    br_white = cli::bg_br_white,
-    yellow1 = ,
+    br_white = cli::col_br_white,
     bright_yellow = ,
-    yellow_br = ,
-    br_yellow = cli::bg_br_yellow,
+    br_yellow = cli::col_br_yellow,
     make_fallback_color(x, bg = TRUE)
   )
 }
@@ -114,56 +89,31 @@ background <- bg
 #' @name stylers
 #' @export
 color <- function(x) {
-  x <- tolower(check_is_string(x))
   switch(
-    x,
-    black0 = ,
+    check_is_string(x),
     black = cli::col_black,
-    blue0 = ,
     blue = cli::col_blue,
-    cyan0 = ,
     cyan = cli::col_cyan,
-    green0 = ,
     green = cli::col_green,
-    magenta0 = ,
     magenta = cli::col_magenta,
-    red0 = ,
     red = cli::col_red,
-    white0 = ,
     white = cli::col_white,
-    yellow0 = ,
     yellow = cli::col_yellow,
-    black1 = ,
     bright_black = ,
-    black_br = ,
     br_black = cli::col_br_black,
-    blue1 = ,
     bright_blue = ,
-    blue_br = ,
     br_blue = cli::col_br_blue,
-    cyan1 = ,
     bright_cyan = ,
-    cyan_br = ,
     br_cyan = cli::col_br_cyan,
-    green1 = ,
     bright_green = ,
-    green_br = ,
     br_green = cli::col_br_green,
-    magenta1 = ,
     bright_magenta = ,
-    magenta_br = ,
     br_magenta = cli::col_br_magenta,
-    red1 = ,
     bright_red = ,
-    red_br = ,
     br_red = cli::col_br_red,
-    white1 = ,
     bright_white = ,
-    white_br = ,
     br_white = cli::col_br_white,
-    yellow1 = ,
     bright_yellow = ,
-    yellow_br = ,
     br_yellow = cli::col_br_yellow,
     make_fallback_color(x)
   )
@@ -179,27 +129,23 @@ colour <- color
 #' @export
 style <- function(x) {
   switch(
-    tolower(check_is_string(x)),
+    check_is_string(x),
     bold = cli::style_bold,
-    dimmed = ,
     dim = cli::style_dim,
-    blur = ,
     blurred = cli::style_blurred,
-    italicized = ,
-    italicize = ,
-    italics = ,
     italic = cli::style_italic,
-    hide = ,
     hidden = cli::style_hidden,
     underline = cli::style_underline,
-    strike = ,
     strikethrough = cli::style_strikethrough,
-    inverted = ,
     inverse = cli::style_inverse,
-    # TODO: Make a function which shows supported styles somewhere. Reference it
-    #       in this error.
     cli::cli_abort(
-      "Can't create style {.val {x}}.",
+      c(
+        "Can't create style {.val {x}}.",
+        i = paste(
+          "{.arg x} must be the string: {.or {c('bold', 'dim', 'blurred'," ,
+          "'italic', 'hidden', 'underline', 'strikethrough', 'inverse')}}."
+        )
+      ),
       class = "vlighter_error"
     )
   )
@@ -222,9 +168,8 @@ make_fallback_color <- function(
     error_call = rlang::caller_env(),
     error_class = "vlightr_error"
   ) {
-  # Taking ownership of the the `cli` error, since the message says a matrix can
-  # be used, which is not true for these style functions. This is a little
-  # dangerous, because there's no "cli_error_bad_color" class.
+  # Taking ownership of the the `cli` error, since the message says a matrix
+  # can be used, which is not true for these style functions.
   rlang::try_fetch(
     cli::make_ansi_style(x, bg = bg),
     rlib_error_3_0 = function(cnd) {
